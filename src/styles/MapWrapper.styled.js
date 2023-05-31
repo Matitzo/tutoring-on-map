b39f3ea8a7f7a22211c6686590f1ef23244ee8ef
Matthew <matitzo@o2.pl>
2023-05-31 17:51:53 +0200

    Fixing map on mobile.

--- src/styles/MapWrapper.styled.js
+++ src/styles/MapWrapper.styled.js
@@ -4,7 +4,15 @@ export const StyledMapWrapper = styled.div`
   width: 45%;
 
   @media (max-width: 1100px) {
-    width: ${({ isMobileMapOn }) => (!isMobileMapOn ? "0px" : "100%")};
+    width: 100%;
+    position: absolute;
+    top: 0;
+    left: 0;
+    z-index: ${({ isMobileMapOn }) => (!isMobileMapOn ? "-1" : "999")};
     visibility: ${({ isMobileMapOn }) => !isMobileMapOn && "hidden"};
   }
 `;
+
+//width: ${({ isMobileMapOn }) => isMobileMapOn && "100%"};
+//visibility: ${({ isMobileMapOn }) => !isMobileMapOn && "hidden"};
+//display: ${({ isMobileMapOn }) => !isMobileMapOn && "none"};
